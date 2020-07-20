b1d045376e9f7250d665ce42159b5af6f16f0f2c
asd <2324124687@qq.com>
2020-07-20 17:56:53 +0800

    主页内容完成

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,74 +1,136 @@
 <template>
-    <div>
-        <nav-bar></nav-bar>
-        <van-tabs v-model="active">
-            <van-tab v-for="(item, index) in category" :key="index" :title="item.title">
-                内容:{{index}}
-            </van-tab>
-        </van-tabs>
+  <div class="home" v-if="category">
+    <nav-bar></nav-bar>
+    <div class="categorytab">
+      <div class="category-ico" @click="$router.push('/editcategory')"><van-icon name="setting-o" /></div>
+      <van-tabs v-model="active" swipeable sticky animated>
+        <van-tab v-for="(item,index) in category" :key="index" :title="item.title">
+          <van-list
+            v-model="item.loading"
+            :immediate-check="false"
+            :finished="item.finished"
+            finished-text="我也是有底线的"
+            @load="onLoad"
+          >
+            <div class="detailparent">
+              <cover
+                class="detailitem"
+                :detailitem="categoryitem"
+                v-for="(categoryitem,categoryindex) in item.list"
+                :key="categoryindex"
+              />
+            </div>
+          </van-list>
+        </van-tab>
+      </van-tabs>
     </div>
+  </div>
 </template>
 
 <script>
-import NavBar from '@/components/common/NavBar.vue'
+import NavBar from "@/components/common/NavBar.vue";
+import cover from "./detail.vue";
 export default {
-    data(){
-        return{
-            category:[],
-            active: 0,
-        }
-    },
-    components:{
-        NavBar
-    },
-    methods:{
-        async selectCategory(){
-            const res = await this.$http.get('/category')
-            
-            this.changeCategory(res.data) 
-        },
-        changeCategory(data){
-            const category1 = data.map((item)=>{
-                item.list =[]
-                item.page = 0
-                item.pagesize = 10
-                return item 
-            });
-            this.category = category1
-        },
-        async selectArticle(){
-            const categoryitem = this.categoryItem()
-            const res = await this.$http.get('/detail/'+categoryitem._id,{
-                params:{
-                    page:categoryitem.page,
-                    pagesize:categoryitem.pagesize
-                }
-            })
-            categoryitem.list.push(...res.data);
-            // categoryitem.list = this.category[this.active]
-            console.log(">>>dd>", categoryitem, res)
-
-        },
-        categoryItem(){
-            const categoryItem = this.category[this.active]
-            console.log('categoryItem', categoryItem)
-            return categoryItem
-        }
-
-    },
-    created(){
-        console.log("执行了")
-        this.selectCategory()
-    },
-    watch:{
-        active(){
-            this.selectArticle()
-        }
+  data() {
+    return {
+      category: [],
+      active: 0
+    };
+  },
+  components: {
+    NavBar,
+    cover
+  },
+  activated() {
+    if(localStorage.getItem('newCat')) {
+        let newCat = JSON.parse(localStorage.getItem('newCat'))
+        this.category = this.changeCategory(newCat)
+        this.selectArticle();
     }
-    
-}
+  },
+  methods: {
+    async selectCategory() {
+      if(localStorage.getItem('newCat')) {
+        return
+      }
+      const res = await this.$http.get("/category");
+      this.category = this.changeCategory(res.data);
+      this.selectArticle();
+    },
+    changeCategory(data) {
+      const category1 = data.map((item, index) => {
+        item.list = [];
+        item.page = 0;
+        item.finished = false;
+        item.loading = true;
+        item.pagesize = 10;
+        return item;
+      });
+      return category1;
+    },
+    async selectArticle() {
+      const categoryitem = this.categoryItem();
+      const res = await this.$http.get("/detail/" + categoryitem._id, {
+        params: {
+          page: categoryitem.page,
+          pagesize: categoryitem.pagesize
+        }
+      });
+      categoryitem.list.push(...res.data);
+      categoryitem.loading = false;
+      if (res.data.length < categoryitem.pagesize) {
+        categoryitem.finished = true;
+      }
+    },
+    onLoad() {
+      const categoryitem = this.categoryItem();
+      setTimeout(() => {
+        categoryitem.page += 1;
+        this.selectArticle();
+      }, 1000);
+    },
+    categoryItem() {
+      const categoryitem = this.category[this.active];
+      return categoryitem;
+    }
+  },
+  watch: {
+    active() {
+      const categoryitem = this.categoryItem();
+      if (!categoryitem.list.length) {
+        this.selectArticle();
+      }
+    }
+  },
+  created() {
+    this.selectCategory();
+  }
+};
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
+.home {
+  background-color: white;
+}
+.detailparent {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  .detailitem {
+    margin: 1.389vw 0;
+    width: 45%;
+  }
+}
+.categorytab{
+  position: relative;
+  .category-ico{
+    position: absolute;
+    z-index: 5;
+    right: 0;
+    top: 1.944vw;
+    padding: 1.389vw 2.778vw;
+    background-color: white;
+  }
+}
 
 </style>
